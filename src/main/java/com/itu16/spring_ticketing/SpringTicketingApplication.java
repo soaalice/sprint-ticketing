97b61cfa6a1dbc06c1798a8c6efcbb6b60a5ad52
soaalice <amboararampanana@gmail.com>
2025-08-24 22:02:14 +0300

    init

---
+++ src/main/java/com/itu16/spring_ticketing/SpringTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.itu16.spring_ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTicketingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itu16/spring_ticketing/SpringTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itu16.spring_ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
